9bcda458cd7cfc460e2ece3f7f6154e4d713dbe8
Escandon <aleskndon@hotmail.com>
2020-09-07 14:39:59 -0500

    comente el metodo del api

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('cosa', 'ThingsController@index');
+// Route::get('cosa', 'ThingsController@index');
