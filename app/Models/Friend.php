16f505e2124b51ca3af8e8da739fa37df5ed134f
Jose Daniel Morales Ocampo <jose-daniel-97@hotmail.com>
2020-09-16 14:33:03 -0500

    enable update party id

--- app/Models/Friend.php
+++ app/Models/Friend.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Friend extends Model
 {
     use HasFactory;
-    protected $fillable = ['name','email','phone','mood'];
+    protected $with = ['party'];
+    protected $fillable = ['name','email','phone','mood','party_id'];
     public function party()
     {
         return $this->belongsTo('App\Models\Party');
